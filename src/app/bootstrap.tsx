558ea07ad44fb8b97eea045ee06cf6da84280a66
Anton-Lavrenko <a.lavrenko@lucky-labs.com>
2016-11-08 13:19:11 +0200

    доделал кнопку и сделал сдвиг (старт анимации)

--- src/app/bootstrap.tsx
+++ src/app/bootstrap.tsx
@@ -4,21 +4,43 @@
 import * as React from "react";
 import ReactDOM from 'react-dom';
 
-import {Button} from "./buttons/button";
+import * as buttons from "./buttons/button";
 
 interface AppProps {}
-interface AppState {}
+interface AppState {
+    top: number;
+}
+
+const Button = buttons.Buttons.Button;
 
 /*
  * A simple React component
  */
 class Application extends React.Component<AppProps, AppState> {
 
+    constructor(props: AppProps, states: AppState){
+        super(props, states);
+
+        this.state = {
+            top: 0
+        }
+
+    }
+
+    onClick(){console.log(1);
+        this.setState({top: this.state.top + 10});
+    }
+
     render() {
+
+        const style = {
+            "margin-top": this.state.top
+        };
+
         return <div>
-            <h1>Hello, ES6 and React 0.13!</h1>
+            <h1 style={style}>Hello, ES6 and React 0.13!</h1>
             <p>
-                <Button color="#000" onClick="">dsdsdsd</Button>
+                <Button color="#000" onClick={this.onClick.bind(this)}>dsdsdsd</Button>
             </p>
         </div>;
     }
--- src/app/buttons/button.tsx
+++ src/app/buttons/button.tsx
@@ -1,70 +1,113 @@
 /// <reference path="../../../typings/react/react-global.d.ts" />
-/// <reference path="./../../interfaces.d.ts"/>
 import * as React from "react";
 
-//var cx = React.addons.classSet;
+//const cx = React.addons.classSet; console.log(cx);
 
 interface ButtonProperties {
     className?  : string;
     color?      : string;
     top?        : number;
     left?       : number;
-    children    : any;
+    children?    : any;
     onClick     : any;
 }
 interface ButtonState {
-
+    classes : string;
 }
+export namespace Buttons {
 
+    /**
+     * Button
+     */
+    export class Button extends React.Component<ButtonProperties, ButtonState> {
 
-/**
- * Button
- */
-export class Button extends React.Component<ButtonProperties, ButtonState> {
+        constructor(props: ButtonProperties, context: any){
+            super(props, context);
 
-    constructor(props: ButtonProperties, context: any){ console.log(context);
-        super(props, context);
+            this.state = {
+                classes : this.props.className ? `button ${this.props.className}` : "button"
+            };
+        }
 
-        this.state = {
-            classes : [this.props.className],
-            active  :  true,
-            hover   :  false,
-            pressed : false
-        };
-    }
+        onMouseDown(){
+            let classes = this.state.classes.split(" ");
 
-    onMouseUp(){
-        this.setState({pressed: false});
-    }
+            if(classes.indexOf("pressed") === -1) {
+                classes.push("pressed");
+            }
 
-    onMouseDown(){
-        this.setState({pressed: true});
-    }
+            this.setState({
+                classes: classes.join(" ")
+            });
 
-    onMouseOver(){
-        this.setState({hover: true});
-    }
+            if(typeof this.props.onClick == "function"){
+                this.props.onClick();
+            }
+        }
 
-    onMouseOut(){
-        this.setState({hover: false});
-    }
+        onMouseUp(){
+            let classes = this.state.classes.split(" ");
 
-    render() {
+            let index = classes.indexOf("pressed");
 
-        const {className, color, top, left, children} = this.props;
+            if(index !== -1) {
+                classes.splice(index, 1);
+            }
 
-        let buttonStyle = {
-            color: color
-        };
+            this.setState({
+                classes: classes.join(" ")
+            });
+        }
 
-        return <button
-            className={cx(this.state.classes)}
-            onMouseOver={this.onMouseOver.bind(this)}
-            onMouseOut={this.onMouseOut.bind(this)}
-            onMouseUp={this.onMouseDown.bind(this)}
-            onMouseDown={this.onMouseDown.bind(this)}
-            style={buttonStyle}>
+        onMouseOver(){
+            let classes = this.state.classes.split(" ");
+
+            if(classes.indexOf("hover") === -1) {
+                classes.push("hover");
+            }
+
+            this.setState({
+                classes: classes.join(" ")
+            });
+        }
+
+        onMouseOut(){
+            let classes = this.state.classes.split(" ");
+
+            let index = classes.indexOf("hover");
+
+            if(index !== -1) {
+                classes.splice(index, 1);
+            }
+
+            let indexPress = classes.indexOf("pressed");
+
+            if(indexPress !== -1) {
+                classes.splice(indexPress, 1);
+            }
+
+            this.setState({
+                classes: classes.join(" ")
+            });
+        }
+
+        render() {
+
+            const {className, color, top, left, children} = this.props;
+
+            let buttonStyle = {
+                color: color
+            };
+
+            return <button
+                className={this.state.classes}
+                onMouseOver={this.onMouseOver.bind(this)}
+                onMouseOut={this.onMouseOut.bind(this)}
+                onMouseUp={this.onMouseUp.bind(this)}
+                onMouseDown={this.onMouseDown.bind(this)}
+                style={buttonStyle}>
                 {children}
-        </button>;
+            </button>;
+        }
     }
 }
